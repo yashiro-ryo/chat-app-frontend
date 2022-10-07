4c405df0fe7b42fe0a605ec5812e13e9ad48b334
yashiroryo <>
2022-10-07 22:06:59 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<div>home page</div>} />
+          <Route path={"/dashboard"} element={<div>dashboard</div>} />
+          <Route path={"/login"} element={<div>ログイン</div>} />
+          <Route
+            path={"/tailwindcss"}
+            element={
+              <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            }
+          />
+          <Route
+            path={"/chakra"}
+            element={<Button colorScheme="blue">Button</Button>}
+          />
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
