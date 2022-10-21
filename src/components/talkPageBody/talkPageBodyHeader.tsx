b5b65c7df1b379d2a468b4d8a21b18af5cd232cb
yashiroryo <>
2022-10-21 20:12:29 +0900

    chore: 急ぎではないUI部分を簡素化

--- src/components/talkPageBody/talkPageBodyHeader.tsx
+++ src/components/talkPageBody/talkPageBodyHeader.tsx
@@ -1,19 +1,14 @@
-import React from 'react'
-import { Box, Link, Flex, Spacer } from "@chakra-ui/react";
-const talkroomName = "山田太郎連盟";
-const talkerNum = 78;
+import React from "react";
+import { Box, Flex, Spacer } from "@chakra-ui/react";
 
 function talkPageBodyHeader() {
   return (
     <>
       <Flex height="50px" border="1px" display="flex">
         <Box height="100%" lineHeight="50px" marginLeft="10px">
-          {talkroomName} ({talkerNum})
+          トークルーム
         </Box>
         <Spacer />
-        <Link lineHeight="50px" marginRight="10px">
-          トーク設定
-        </Link>
       </Flex>
     </>
   );
